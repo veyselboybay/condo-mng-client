1b500e99e749cf8d2025b060c2d11faa0d2c97ab
veyselboybay <veysel.boybay@outlook.com>
2023-05-30 16:19:52 -0400

    post page added with mock data

--- components/Posts/index.jsx
+++ components/Posts/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Post from '../Post'
+import { Container } from 'react-bootstrap'
 
 const Posts = () => {
     return (
-        <div>Posts</div>
+        <div style={{ maxHeight: '90vh', overflowY: 'auto' }}>
+            <Post />
+            <Post />
+            <Post />
+        </div>
     )
 }
 
